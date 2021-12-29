36717239dfff2812bb676fdd7d62402484d8ad73
Ricky Roy <rickyroynardson@gmail.com>
2021-12-29 16:56:37 +0700

    update authorization

--- app/Http/Controllers/AdminBannerController.php
+++ app/Http/Controllers/AdminBannerController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Banner;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Storage;
 
 class AdminBannerController extends Controller
 {
     public function index()
     {
-        return view('admin.banner.index', [
-            'title' => 'Banner'
-        ]);
+        if (Gate::allows('5_1')) {
+            return view('admin.banner.index', [
+                'title' => 'Banner'
+            ]);
+        } else {
+            return redirect()->route('admin.error-401');
+        }
     }
 
     public function table()
@@ -26,69 +31,89 @@ class AdminBannerController extends Controller
 
     public function add()
     {
-        return view('admin.banner.add', [
-            'title' => 'Add Banner'
-        ]);
+        if (Gate::allows('5_2')) {
+            return view('admin.banner.add', [
+                'title' => 'Add Banner'
+            ]);
+        } else {
+            return redirect()->route('admin.error-401');
+        }
     }
 
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => ['required'],
-            'photo' => ['required', 'image', 'file', 'max:2048']
-        ], [
-            'name.required' => 'Name is required.',
-            'photo.required' => 'Photo is required.',
-            'photo.image' => 'Photo must be an image.',
-            'photo.file' => 'Photo must be an file.'
-        ]);
+        if (Gate::allows('5_2')) {
+            $validatedData = $request->validate([
+                'name' => ['required'],
+                'photo' => ['required', 'image', 'file', 'max:2048']
+            ], [
+                'name.required' => 'Name is required.',
+                'photo.required' => 'Photo is required.',
+                'photo.image' => 'Photo must be an image.',
+                'photo.file' => 'Photo must be an file.'
+            ]);
 
-        if ($request->file('photo')) {
-            $fileName = 'e-hotel-' . time() . '.' . $request->file('photo')->extension();
-            Storage::putFileAs('banners-photo', $request->file('photo'), $fileName);
-            $validatedData['photo'] = $fileName;
+            if ($request->file('photo')) {
+                $fileName = 'e-hotel-' . time() . '.' . $request->file('photo')->extension();
+                Storage::putFileAs('banners-photo', $request->file('photo'), $fileName);
+                $validatedData['photo'] = $fileName;
+            }
+
+            Banner::create($validatedData);
+            return response()->json(['message' => 'Data saved successfully!']);
+        } else {
+            return redirect()->route('admin.error-401');
         }
-
-        Banner::create($validatedData);
-        return response()->json(['message' => 'Data saved successfully!']);
     }
 
     public function edit($id)
     {
-        return view('admin.banner.edit', [
-            'title' => 'Edit Banner',
-            'banner' => Banner::find($id)
-        ]);
+        if (Gate::allows('5_3')) {
+            return view('admin.banner.edit', [
+                'title' => 'Edit Banner',
+                'banner' => Banner::find($id)
+            ]);
+        } else {
+            return redirect()->route('admin.error-401');
+        }
     }
 
     public function update(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => ['required'],
-            'photo' => ['image', 'file', 'max:2048'],
-        ], [
-            'name.required' => 'Name is required.',
-            'photo.image' => 'Photo must be an image.',
-            'photo.file' => 'Photo must be an file.'
-        ]);
+        if (Gate::allows('5_3')) {
+            $validatedData = $request->validate([
+                'name' => ['required'],
+                'photo' => ['image', 'file', 'max:2048'],
+            ], [
+                'name.required' => 'Name is required.',
+                'photo.image' => 'Photo must be an image.',
+                'photo.file' => 'Photo must be an file.'
+            ]);
 
-        if ($request->file('photo')) {
-            $fileName = 'e-hotel-' . time() . '.' . $request->file('photo')->extension();
-            Storage::putFileAs('banners-photo', $request->file('photo'), $fileName);
-            Storage::delete('banners-photo/' . $request->oldPhoto);
-            $validatedData['photo'] = $fileName;
+            if ($request->file('photo')) {
+                $fileName = 'e-hotel-' . time() . '.' . $request->file('photo')->extension();
+                Storage::putFileAs('banners-photo', $request->file('photo'), $fileName);
+                Storage::delete('banners-photo/' . $request->oldPhoto);
+                $validatedData['photo'] = $fileName;
+            }
+
+            Banner::find($request->id)->update($validatedData);
+            return response()->json(['message' => 'Data updated successfully!']);
+        } else {
+            return redirect()->route('admin.error-401');
         }
-
-        Banner::find($request->id)->update($validatedData);
-        return response()->json(['message' => 'Data updated successfully!']);
     }
 
     public function delete($id)
     {
-        if (Banner::find($id)->photo) {
-            Storage::delete('banners-photo/' . Banner::find($id)->photo);
+        if (Gate::allows('5_4')) {
+            if (Banner::find($id)->photo) {
+                Storage::delete('banners-photo/' . Banner::find($id)->photo);
+            }
+            Banner::destroy($id);
+            return response()->json(['message' => 'Data deleted successfully!']);
+        } else {
+            return redirect()->route('admin.error-401');
         }
-        Banner::destroy($id);
-        return response()->json(['message' => 'Data deleted successfully!']);
     }
 }
